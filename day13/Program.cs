6bca43edb232fb2ed53353abd69735b8d14b9dcc
UlfWallin <herr.wallin@gmail.com>
2024-11-17 08:50:22 +0100

    Day 13

---
+++ day13/Program.cs
@@ -0,0 +1,12 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var timestamp = int.Parse(lines[0]);
+var nextBus = lines[1]
+    .Split(',')
+    .Where(l => l != "x")
+    .Select(int.Parse)
+    .Select(id => (Id: id, Time: id * ((timestamp / id) + 1)))
+    .OrderBy(b => b.Time)
+    .First();
+
+var result = nextBus.Id * (nextBus.Time - timestamp);
+Console.WriteLine(result);
